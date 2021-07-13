6e7d8650e6262d75becd568d56f6e6c110cd574f
Nate Denys <nate_denys@yahoo.com>
2021-07-13 14:44:04 -0700

    initial upload

---
+++ precache-manifest.f8c2456d03930f7ceeafc59f13522877.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "afbb64a2098e8349fcd2",
+    "url": "/kaori_sushi/css/about.4e89e1d6.css"
+  },
+  {
+    "revision": "9cd6b6218d5ced08bfd6",
+    "url": "/kaori_sushi/css/chunk-vendors.935883d2.css"
+  },
+  {
+    "revision": "9c93359ba0bb5ad147bed0426c61e095",
+    "url": "/kaori_sushi/img/indoor.9c93359b.jpg"
+  },
+  {
+    "revision": "ebcfed4faf95ad27be7f763f20461e44",
+    "url": "/kaori_sushi/index.html"
+  },
+  {
+    "revision": "afbb64a2098e8349fcd2",
+    "url": "/kaori_sushi/js/about.a02a00db.js"
+  },
+  {
+    "revision": "af5703e49ecd10193788",
+    "url": "/kaori_sushi/js/app.bb1e9d36.js"
+  },
+  {
+    "revision": "9cd6b6218d5ced08bfd6",
+    "url": "/kaori_sushi/js/chunk-vendors.89b1e805.js"
+  },
+  {
+    "revision": "d17e3efa381f6a4f14a1e3ca2b07000e",
+    "url": "/kaori_sushi/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/kaori_sushi/robots.txt"
+  }
+]);
